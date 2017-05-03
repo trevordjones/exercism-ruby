476dd669b39d2e6989056d9ac408a2d3e919e507
Trevor Jones <trevordavidjones@gmail.com>
2017-05-03 12:29:54 -0400

    RNA transcription

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,23 @@
+class Complement
+  COMPLEMENT = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
+  class << self
+    def of_dna(strand)
+      complement = ''
+      strand.split('').each do |letter|
+        return '' if COMPLEMENT[letter].nil?
+        complement += COMPLEMENT[letter]
+      end
+      complement
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
